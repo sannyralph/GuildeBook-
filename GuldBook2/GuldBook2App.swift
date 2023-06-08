4d2534f5b238c42c237f484f9e598be2ef6b37d0
sannyralph <sannylauren@gmail.com>
2023-06-08 15:43:36 -0400

    Initial Commit

---
+++ GuldBook2/GuldBook2App.swift
@@ -0,0 +1,17 @@
+//
+//  GuldBook2App.swift
+//  GuldBook2
+//
+//  Created by SANNI ABDULRAFIU on 08/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct GuldBook2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
